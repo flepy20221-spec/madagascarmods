4d066dce9b8db0d776cdbff70d08a6d483f753ab
flepy20221-spec <3.10415489e+08+flepy20221-spec@users.noreply.github.com>
2026-08-13 18:39:42 +0000

    chore: remover scripts de debug acidentais

--- debug_inject2.py
+++
@@ -1,8 +0,0 @@
-test_path = '/home/ubuntu/madagascarmods/madagascarmods-backend/tests/asaas_auth_webhook.test.js'
-t = open(test_path).read()
-old = "console.log('handler name:', webhookHandler.name);"
-new = "console.log('handler src head:', webhookHandler.toString().slice(0, 80));"
-assert old in t, 'marker not found'
-t = t.replace(old, new, 1)
-open(test_path, 'w').write(t)
-print('patched')
